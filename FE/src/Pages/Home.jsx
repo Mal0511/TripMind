0abf09c0a0c99937d47e8871cd01028ee9f9f891
VanLam <vanlam10052003@example.com>
2025-09-25 11:57:46 +0700

    feature: tao api ket noi db trip

--- FE/src/Pages/Home.jsx
+++ FE/src/Pages/Home.jsx
@@ -1,44 +1,20 @@
 import DestinationCard from "../Components/DestinationCard";
 import { useEffect, useState } from "react";
 
+const Home = () => {
+  const [destinations, setDestinations] = useState([]);
 
-export default function Home() {
-  const [trips, setTrips] = useState([]);
-
-   useEffect(() => {
-    fetch("http://localhost:1150/api/trip", { credentials: "include" })
-      .then(res => {
+  useEffect(() => {
+    fetch("http://localhost:1105/api/trip") // API backend
+      .then((res) => {
         if (res.status === 401) {
-          window.location.href = "http://localhost:1150/";
+          window.location.href = "http://localhost:1150/"; // redirect login
         }
         return res.json();
       })
-      .then(data => setTrips(data))
-      .catch(err => console.error(err));
+      .then((data) => setDestinations(data))
+      .catch((err) => console.error(err));
   }, []);
-
-
-
-  const destinations = [
-    {
-      image: "/public/assets/P1.jpg",
-      title: "Ta Xua",
-      price: "$1.5k",
-      days: "6 Days Trip",
-    },
-    {
-      image: "/public/assets/P2.jpg",
-      title: "Da Lat",
-      price: "$1.5k",
-      days: "5 Days Trip",
-    },
-    {
-      image: "/public/assets/P3.jpg",
-      title: "Phong Nha",
-      price: "$1.5k",
-      days: "7 Days Trip",
-    },
-  ];
 
   return (
     <section className="max-w-6xl px-6 py-12 mx-auto">
@@ -59,4 +35,6 @@ export default function Home() {
       </div>
     </section>
   );
-}
+};
+
+export default Home;
--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,8 @@
 import express from "express";
 import homeControler from "../controllers/homeController";
 import LoginController from "../controllers/LoginController";
+import getAllTrips from "../controllers/TripController";
+
 
 let router = express.Router();
 
@@ -9,7 +11,6 @@ let initWebRoutes = (app) => {
     router.get("/", LoginController.getLoginPage);
     router.post("/auth/login", LoginController.getLoginController);
     router.post("/auth/register", LoginController.getRegisterController);
-    
     return app.use("/", router);
 }
 module.exports = initWebRoutes;
